d80b5ee4e145b0b7240fa5ec2b28ff7a8ec0203d
ashan <uhashan17929@gmail.com>
2024-04-06 21:27:20 +0530

    Create My Experience

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
+import Experience from "./components/Experience";
 
 
 
@@ -13,10 +14,13 @@ function App() {
       <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       </div>
       <div className=" container mx-auto px-8">
+
       <Navbar/>
       <Hero/>
       <About/>
       <Skills/>
+      <Experience/>
+      
       </div>
     </div>
   );
