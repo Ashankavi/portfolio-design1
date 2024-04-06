4d84e8f5281984d82b0f48602991048878e06a30
ashan <uhashan17929@gmail.com>
2024-04-06 19:23:11 +0530

    Create Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+// Icons
+
+import {FaLinkedin} from "react-icons/fa"
+import {FaGithub} from "react-icons/fa"
+import {FaInstagram} from "react-icons/fa"
+import { FaDribbble } from "react-icons/fa";
+
+
+
+
+// Images
+
+import Logo from "../assets/kevinRushLogo.png"
+
+const Navbar = () => {
+  return (
+    <div>
+        <nav className=" mb=20 flex items-center justify-between py-6">
+            <div className='flex flex-shrink-0 items-center'>
+                <img className=' mx-2 w-10' src={Logo} alt="logo" />
+            </div>
+            <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
+                <FaGithub/>
+                <FaLinkedin/>
+                <FaInstagram/>
+                <FaDribbble/>
+
+
+            </div>
+    
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
